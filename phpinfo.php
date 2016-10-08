bfbc16cfc9b7ace7896cca3af7f118ffcc216b13
Khaled Mohammed <khaled.mak94@gmail.com>
2016-10-08 23:06:31 +0530

    added test.php phpinfo.php

---
+++ phpinfo.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user-1
+ * Date: 10/8/2016
+ * Time: 11:01 PM
+ */
+phpinfo();
---
+++ test.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <title>PHP</title>
+</head>
+<body>
+<p>
+    <?php
+        echo "hello World";
+    ?>
+</p>
+</body>
+</html>
